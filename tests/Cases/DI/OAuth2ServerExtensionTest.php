6f7fa26a370a42f469dc096957a2211d1fb725d5
Felix <rkfelix@gmail.com>
2025-12-14 19:41:48 +0000

    Update tests
    - Rename fixtures to Fixtures (PSR-4 compliance)
    - Add tests for custom TTL configuration
    - Simplify bootstrap.php

--- tests/Cases/DI/OAuth2ServerExtensionTest.php
+++ tests/Cases/DI/OAuth2ServerExtensionTest.php
@@ -6,6 +6,11 @@ use Contributte\OAuth2Server\DI\OAuth2ServerExtension;
 use Contributte\Tester\Environment;
 use Contributte\Tester\Toolkit;
 use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Grant\AuthCodeGrant;
+use League\OAuth2\Server\Grant\ClientCredentialsGrant;
+use League\OAuth2\Server\Grant\ImplicitGrant;
+use League\OAuth2\Server\Grant\PasswordGrant;
+use League\OAuth2\Server\Grant\RefreshTokenGrant;
 use League\OAuth2\Server\ResourceServer;
 use Nette\DI\Compiler;
 use Nette\DI\Container;
@@ -15,6 +20,7 @@ use Tester\FileMock;
 
 require_once __DIR__ . '/../../bootstrap.php';
 
+// Test basic configuration without grants
 Toolkit::test(function (): void {
 	$loader = new ContainerLoader(Environment::getTmpDir(), true);
 	$class = $loader->load(function (Compiler $compiler): void {
@@ -23,11 +29,11 @@ Toolkit::test(function (): void {
 			oauth2.server:
 				encryptionKey: "fake"
 				privateKey:
-					path: "../../fixtures/keys/private.key"
+					path: "../../Fixtures/keys/private.key"
 					passPhrase: "foo"
 					permissionCheck: false
 				publicKey:
-					path: "../../fixtures/keys/public.key"
+					path: "../../Fixtures/keys/public.key"
 					permissionCheck: false
 
 			services:
@@ -47,6 +53,7 @@ Toolkit::test(function (): void {
 	Assert::count(1, $container->findByType(ResourceServer::class));
 });
 
+// Test encryption key as Statement
 Toolkit::test(function (): void {
 	$loader = new ContainerLoader(Environment::getTmpDir(), true);
 	$class = $loader->load(function (Compiler $compiler): void {
@@ -55,11 +62,11 @@ Toolkit::test(function (): void {
 			oauth2.server:
 				encryptionKey: Defuse\Crypto\Key::loadFromAsciiSafeString("fake")
 				privateKey:
-					path: "../../fixtures/keys/private.key"
+					path: "../../Fixtures/keys/private.key"
 					passPhrase: "foo"
 					permissionCheck: false
 				publicKey:
-					path: "../../fixtures/keys/public.key"
+					path: "../../Fixtures/keys/public.key"
 					permissionCheck: false
 
 			services:
@@ -77,4 +84,160 @@ Toolkit::test(function (): void {
 
 	Assert::count(1, $container->findByType(AuthorizationServer::class));
 	Assert::count(1, $container->findByType(ResourceServer::class));
+});
+
+// Test grants with default TTL
+Toolkit::test(function (): void {
+	$loader = new ContainerLoader(Environment::getTmpDir(), true);
+	$class = $loader->load(function (Compiler $compiler): void {
+		$compiler->addExtension('oauth2.server', new OAuth2ServerExtension());
+		$compiler->loadConfig(FileMock::create('
+			oauth2.server:
+				encryptionKey: "fake"
+				privateKey:
+					path: "../../Fixtures/keys/private.key"
+					passPhrase: "foo"
+					permissionCheck: false
+				publicKey:
+					path: "../../Fixtures/keys/public.key"
+					permissionCheck: false
+				grants:
+					clientCredentials: []
+					password: []
+					refreshToken: []
+
+			services:
+				- Tests\Fixtures\Repositories\ClientRepository
+				- Tests\Fixtures\Repositories\AccessTokenRepository
+				- Tests\Fixtures\Repositories\ScopeRepository
+				- Tests\Fixtures\Repositories\AuthCodeRepository
+				- Tests\Fixtures\Repositories\RefreshTokenRepository
+				- Tests\Fixtures\Repositories\UserRepository
+		', 'neon'));
+	}, [getmypid(), 3]);
+
+	/** @var Container $container */
+	$container = new $class();
+
+	Assert::count(1, $container->findByType(AuthorizationServer::class));
+	Assert::count(1, $container->findByType(ClientCredentialsGrant::class));
+	Assert::count(1, $container->findByType(PasswordGrant::class));
+	Assert::count(1, $container->findByType(RefreshTokenGrant::class));
+});
+
+// Test grants with custom TTL (issue #14)
+Toolkit::test(function (): void {
+	$loader = new ContainerLoader(Environment::getTmpDir(), true);
+	$class = $loader->load(function (Compiler $compiler): void {
+		$compiler->addExtension('oauth2.server', new OAuth2ServerExtension());
+		$compiler->loadConfig(FileMock::create('
+			oauth2.server:
+				encryptionKey: "fake"
+				privateKey:
+					path: "../../Fixtures/keys/private.key"
+					passPhrase: "foo"
+					permissionCheck: false
+				publicKey:
+					path: "../../Fixtures/keys/public.key"
+					permissionCheck: false
+				grants:
+					clientCredentials:
+						ttl: PT30M
+					password:
+						ttl: PT1H
+					refreshToken:
+						ttl: P7D
+
+			services:
+				- Tests\Fixtures\Repositories\ClientRepository
+				- Tests\Fixtures\Repositories\AccessTokenRepository
+				- Tests\Fixtures\Repositories\ScopeRepository
+				- Tests\Fixtures\Repositories\AuthCodeRepository
+				- Tests\Fixtures\Repositories\RefreshTokenRepository
+				- Tests\Fixtures\Repositories\UserRepository
+		', 'neon'));
+	}, [getmypid(), 4]);
+
+	/** @var Container $container */
+	$container = new $class();
+
+	Assert::count(1, $container->findByType(AuthorizationServer::class));
+	Assert::count(1, $container->findByType(ClientCredentialsGrant::class));
+	Assert::count(1, $container->findByType(PasswordGrant::class));
+	Assert::count(1, $container->findByType(RefreshTokenGrant::class));
+});
+
+// Test authCode grant with all options
+Toolkit::test(function (): void {
+	$loader = new ContainerLoader(Environment::getTmpDir(), true);
+	$class = $loader->load(function (Compiler $compiler): void {
+		$compiler->addExtension('oauth2.server', new OAuth2ServerExtension());
+		$compiler->loadConfig(FileMock::create('
+			oauth2.server:
+				encryptionKey: "fake"
+				privateKey:
+					path: "../../Fixtures/keys/private.key"
+					passPhrase: "foo"
+					permissionCheck: false
+				publicKey:
+					path: "../../Fixtures/keys/public.key"
+					permissionCheck: false
+				grants:
+					authCode:
+						ttl: PT1H
+						authCodeTTL: PT5M
+						codeExchangeProof: true
+
+			services:
+				- Tests\Fixtures\Repositories\ClientRepository
+				- Tests\Fixtures\Repositories\AccessTokenRepository
+				- Tests\Fixtures\Repositories\ScopeRepository
+				- Tests\Fixtures\Repositories\AuthCodeRepository
+				- Tests\Fixtures\Repositories\RefreshTokenRepository
+				- Tests\Fixtures\Repositories\UserRepository
+		', 'neon'));
+	}, [getmypid(), 5]);
+
+	/** @var Container $container */
+	$container = new $class();
+
+	Assert::count(1, $container->findByType(AuthorizationServer::class));
+	Assert::count(1, $container->findByType(AuthCodeGrant::class));
+});
+
+// Test implicit grant with accessTokenTTL
+Toolkit::test(function (): void {
+	$loader = new ContainerLoader(Environment::getTmpDir(), true);
+	$class = $loader->load(function (Compiler $compiler): void {
+		$compiler->addExtension('oauth2.server', new OAuth2ServerExtension());
+		$compiler->loadConfig(FileMock::create('
+			oauth2.server:
+				encryptionKey: "fake"
+				privateKey:
+					path: "../../Fixtures/keys/private.key"
+					passPhrase: "foo"
+					permissionCheck: false
+				publicKey:
+					path: "../../Fixtures/keys/public.key"
+					permissionCheck: false
+				grants:
+					implicit:
+						ttl: PT2H
+						accessTokenTTL: PT15M
+
+			services:
+				- Tests\Fixtures\Repositories\ClientRepository
+				- Tests\Fixtures\Repositories\AccessTokenRepository
+				- Tests\Fixtures\Repositories\ScopeRepository
+				- Tests\Fixtures\Repositories\AuthCodeRepository
+				- Tests\Fixtures\Repositories\RefreshTokenRepository
+				- Tests\Fixtures\Repositories\UserRepository
+		', 'neon'));
+	}, [getmypid(), 6]);
+
+	/** @var Container $container */
+	$container = new $class();
+
+	Assert::count(1, $container->findByType(AuthorizationServer::class));
+	Assert::count(1, $container->findByType(ImplicitGrant::class));
 });
